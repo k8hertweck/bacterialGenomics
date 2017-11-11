d6e6cab0e687fc537ca538c1412bd3bccb771f88
k8hertweck <k8hertweck@gmail.com>
2017-11-11 17:23:03 -0600

    generalizing script use

--- projectSetup.sh
+++ projectSetup.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 ## installing software and downloading data
+
 # usage: bash setup.sh URL_R1 URL_R2
 
+# assign variable for location of project directory
 PROJECT=`pwd`
 
 # download and install trimmomatic
-cd
+cd # change to home directory (appropriate for software installation)
 wget http://www.usadellab.org/cms/uploads/supplementary/Trimmomatic/Trimmomatic-0.36.zip
 # if you already have trimmomatic installed, select A to overwrite
 unzip Trimmomatic-0.36.zip
@@ -17,10 +19,11 @@ mkdir data
 cd data
 
 # download data from URL
-wget https://www.dropbox.com/s/y1ryexxtogx8670/Cace-463_S1_L001_R1_001.fastq.gz
-wget https://www.dropbox.com/s/rkd6qr15orcan8i/Cace-463_S1_L001_R2_001.fastq.gz
+wget $1
+wget $2
 
 ## check data download: view first few lines of each fastq file
-# zcat prints contents of file to screen if file is gzipped
+# zcat prints contents of file to screen if file is gzipped 
 #zcat $R1 | head  
 #zcat $R2 | head
+# zcat works in GitBash and Linux (e.g., cloud instances), but Macs use gzcat instead
