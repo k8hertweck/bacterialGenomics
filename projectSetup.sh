821f24fa64cb5d21ce75b3e2098613c70a2ba46f
k8hertweck <k8hertweck@gmail.com>
2017-11-12 19:12:52 -0600

    adding reference download and more comments

--- projectSetup.sh
+++ projectSetup.sh
@@ -3,11 +3,14 @@
 ## installing software and downloading data
 
 # usage: bash setup.sh URL_R1 URL_R2
+# URL_R1 and URL_R2 represent the names of your two data files (forward and reverse reads)
+# URLs should have been sent to you via email
 
 # assign variable for location of project directory
 PROJECT=`pwd`
 
 # download and install trimmomatic
+# trimmomatic is the only software we'll use that is not already installed in BioLinux
 cd # change to home directory (appropriate for software installation)
 echo " SETTING UP TRIMMOMATIC"
 wget http://www.usadellab.org/cms/uploads/supplementary/Trimmomatic/Trimmomatic-0.36.zip
@@ -22,7 +25,7 @@ echo "SETTING UP DATA DIRECTORY"
 cd $PROJECT
 mkdir data
 
-# download data from URL
+# download sequence data (fastqc) from URL
 echo "DOWNLOADING DATA"
 wget $1
 wget $2
@@ -35,3 +38,9 @@ mv *.fastq.gz data/
 #		echo $x
 #		zcat $x | head
 #done
+
+# download reference sequence 
+# this file was too large to include on GitHub with the other references
+echo "DOWNLOADING CLOSTRIDIUM REFERENCE"
+wget https://www.dropbox.com/s/ty475u1rf9qwygi/CaceATCC824.fas
+mv CaceATCC824.fas reference/
